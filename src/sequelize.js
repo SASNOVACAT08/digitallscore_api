6344e6471b19acf625cd39a802b234d20933cce7
Ruben <atosc08@gmail.com>
2020-05-06 07:55:43 +0200

    Dotenv

--- src/sequelize.js
+++ src/sequelize.js
@@ -1,31 +1,42 @@
-const Sequelize = require('sequelize');
+require("dotenv").config();
+const Sequelize = require("sequelize");
 
 module.exports = function (app) {
-  const connectionString = app.get('mysql');
+  const connectionString =
+    "mysql://" +
+    process.env.DB_USERNAME +
+    ":" +
+    process.env.DB_PASSWORD +
+    "@" +
+    process.env.DB_ADDRESS +
+    ":" +
+    process.env.DB_PORT +
+    "/" +
+    process.env.DB_NAME;
   const sequelize = new Sequelize(connectionString, {
-    dialect: 'mysql',
+    dialect: "mysql",
     logging: false,
     define: {
-      freezeTableName: true
-    }
+      freezeTableName: true,
+    },
   });
   const oldSetup = app.setup;
 
-  app.set('sequelizeClient', sequelize);
+  app.set("sequelizeClient", sequelize);
 
   app.setup = function (...args) {
     const result = oldSetup.apply(this, args);
 
     // Set up data relationships
     const models = sequelize.models;
-    Object.keys(models).forEach(name => {
-      if ('associate' in models[name]) {
+    Object.keys(models).forEach((name) => {
+      if ("associate" in models[name]) {
         models[name].associate(models);
       }
     });
 
     // Sync to the database
-    app.set('sequelizeSync', sequelize.sync());
+    app.set("sequelizeSync", sequelize.sync());
 
     return result;
   };
